2e9790a9be5eef614c5af41bb8b4c153b67f9eb4
Tafadzwa <itafadzwa28@gmail.com>
2026-04-27 07:33:33 +0200

    icons configuration(splash and launcher icons)

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'QR Code Generator',
